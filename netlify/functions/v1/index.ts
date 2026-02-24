176d5c2eda1dbb89d890d470d42af2634ddeb6d3
donavon <github@donavon.com>
2026-02-24 15:51:50 -0500

    add temp logging

--- netlify/functions/v1/index.ts
+++ netlify/functions/v1/index.ts
@@ -1,11 +1,20 @@
+// oxlint-disable no-console
 import type { Context } from "@netlify/functions";
 import { getData } from "./get-data";
 import { responseJson } from "~/lib/response-json";
 
 export default async function handler(
   request: Request,
-  _context: Context,
+  context: Context,
 ): Promise<Response> {
+  const { pathname } = new URL(request.url);
+
+  const ua = request.headers.get("user-agent");
+  const who = request.headers.get("x-who");
+  const ip = context.ip;
+  const method = request.method;
+  console.log({ ip, method, pathname, who, ua });
+
   const data = await getData();
 
   return responseJson(data, request);
